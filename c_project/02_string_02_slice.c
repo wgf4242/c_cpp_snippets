40c967e89ae940e2ebe8478a5230d61333b36594
wgf4242 <wgf4242@gmail.com>
2024-10-16 10:32:16 +0800

    sync

--- c_project/02_string_02_slice.c
+++ c_project/02_string_02_slice.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+void slice16_memcpy() {
+    char original[32] = "12345678abcdefghABCDEFGH87654321";
+    char result[17];
+    memcpy(result, original, 16);
+    result[16] = '\0';
+    printf("截取的字符串: %s\n", result);
+}
+
+void slice16_strncpy() {
+    char original[32] = "12345678abcdefghABCDEFGH87654321";
+    char result[17];
+    strncpy(result, original, 16);
+    result[16] = '\0';
+    printf("截取的字符串: %s\n", result);
+}
+
 int main() {
     char src[40];
     char dest[12];
