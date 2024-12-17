34352ad205e131ca4556664788dddfb8c2bc0c17
wgf4242 <wgf4242@gmail.com>
2024-12-18 07:05:53 +0800

    sync

---
+++ Windows/examples/a019_DLL_Hook_01/native/dllmain.cpp
@@ -0,0 +1,30 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+
+#include <windows.h>
+#include <stdio.h>
+
+void InstallHook() {
+	printf("Hook Installed\n");
+}
+
+void RemoveHook() {
+	printf("Hook Removed\n");
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		InstallHook();
+		break;
+	case DLL_PROCESS_DETACH:
+		RemoveHook();
+		break;
+	}
+	return TRUE;
+}
